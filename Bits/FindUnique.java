642124f0df93c2c215a174bbbb96624a9f475715
Prem Krishna Chettri <hprem991@gmail.com>
2017-08-13 20:26:18 -0700

    Finding Unique element among 3 occurance

---
+++ Bits/FindUnique.java
@@ -0,0 +1,37 @@
+package Bits;
+	
+import java.util.*;
+import java.lang.*;
+
+//Given an array where every element occurs three times, except one element which occurs only once
+//http://www.geeksforgeeks.org/find-the-element-that-appears-once/
+
+/*
+ * 
+Let us consider the example array {5, 5, 5, 8}. The 101, 101, 101, 1000
+Sum of first bits%3 = (1 + 1 + 1 + 0)%3 = 0;
+Sum of second bits%3 = (0 + 0 + 0 + 0)%0 = 0;
+Sum of third bits%3 = (1 + 1 + 1 + 0)%3 = 0;
+Sum of fourth bits%3 = (1)%3 = 1;
+Hence number which appears once is 1000
+ */
+
+public class FindUnique {
+	public int getSingle(int arr[], int n){
+		int result = 0;
+		int counter = 0;
+		int bitSet = 0;
+		// Position Counter
+		for(int i=0; i < 32; i++){ // Assuming its 32 bit Integer collection
+			counter = 0;
+			bitSet = 1 << i; 
+			for(int elem = 0; elem < n; elem++){
+				if((bitSet & arr[elem]) == 0)
+					counter++;
+			}
+			if((counter % 3) == 0) 
+				result |= bitSet;
+		}
+		return result;
+	}
+}
